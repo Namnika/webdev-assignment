22a0fffdcbd75d0fe96867070b801f408ab9b438
Namnika <janbandhunamnika@gmail.com>
2025-09-01 00:06:50 +0530

    update dasboard with components and user's charts

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,42 +1,43 @@
 import React from 'react'
-import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
 import AvatarChart from '../components/charts/AvatarChart'
 import UsersPerDayChart from '../components/charts/UsersPerDayChart'
 import UserCard from '../components/UserCard'
 import SignupChart from '../components/charts/SignupChart'
+import ChartCard from '../components/ChartCard'
+import RecentlyJoinedChart from '../components/charts/RecentlyJoinedChart'
 
 function Dashboard() {
-  return (
-    <div className='d-flex border flex-column mb-3'>
-        <Sidebar />
-        <div class="flex-grow-1 border p-4">
-         <Navbar />
-        </div>
-        <div className='container border text-center gap-3'>
-            <div className='row'>
-                <div className='col border'>
-                    <UserCard title="Total Users" value="1,452"/>
-                </div>
-                <div className='col border'>
-                    <UserCard title="Active Users" value="870"/>
-                </div>
-                <div className='col border'>
-                    <UserCard title="New Signups" value="120"/>
+    return (
+        <div className='d-flex flex-column mb-3'>
+            <div class="flex-grow-1 ">
+                <Navbar />
+            </div>
+            <div className=' text-center d-flex flex-col align-items-center justify-content-center  mt-3 m-2'>
+                <UserCard title="Total Users" value="1,452" />
+            </div>
+            <div className='container text-center gap-3'>
+                <div className='row'>
+                    <div className='col'>
+                        {/* chartCard wrap each chart */}
+                        <ChartCard title="Users created per day (Last 30 days)">
+                            <UsersPerDayChart />
+                        </ChartCard>
+                        <ChartCard title="Avatar Distribution">
+                            <AvatarChart />
+                        </ChartCard>
+                        <ChartCard title="Most Active Signup today">
+                            <SignupChart />
+                        </ChartCard>
+                        <ChartCard title="Recently Joined Users">
+                            <RecentlyJoinedChart />
+                        </ChartCard>
+
+                    </div>
                 </div>
             </div>
         </div>
-        <div className='container border text-center gap-3'>
-             <div className='row'>
-                <div className='col'>
-                    <AvatarChart />
-                    <SignupChart />
-                    <UsersPerDayChart />
-                </div>
-             </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Dashboard
