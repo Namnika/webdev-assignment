c0a705be6ae31268790262dd974395f4f0ef709c
Namnika <janbandhunamnika@gmail.com>
2025-09-01 00:44:30 +0530

    modifiy chart avatar sizes

--- src/components/ChartCard.js
+++ src/components/ChartCard.js
@@ -4,9 +4,9 @@ function ChartCard({title, children}) {
   return (
     <div className='chart-card mb-2'>
         <h2 className='chart-card-title'>{title}</h2>
-        <p style={{height: "64px"}}>
+        <div style={{height: "64px"}}>
             {children}
-        </p>
+        </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
-function Navbar() {
+function Navbar({avatar}) {
   return (
     <div className='navbar d-flex justify-content-between align-items-center p-4 text-white shadow-sm'>
       <div className='fs-4 fw-bold mx-2'>Users Dashboard</div>
 
       <div className='d-flex align-items-center gap-4'>
-        <select className='days-select' name="" id="">
+        <select className='days-select' name="days-select" id="days-select">
           <option value="7">Last 7 days</option>
           <option value="30">Last 30 days</option>
         </select>
       </div>
 
       <div className='user-avatar'>
-        <span>U</span>
+        <span><img alt='user-avatar' className='user-avatar' src={avatar}/></span>
       </div>
     </div>
   )
