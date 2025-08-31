2e4fa1ccbc1f754b6ef05b64889753331f296395
Namnika <janbandhunamnika@gmail.com>
2025-09-01 00:04:43 +0530

    add Navbar with user avatar fields

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar d-flex justify-content-between align-items-center p-4 text-white shadow-sm'>
+      <div className='fs-4 fw-bold mx-2'>Users Dashboard</div>
+
+      <div className='d-flex align-items-center gap-4'>
+        <select className='days-select' name="" id="">
+          <option value="7">Last 7 days</option>
+          <option value="30">Last 30 days</option>
+        </select>
+      </div>
+
+      <div className='user-avatar'>
+        <span>U</span>
+      </div>
+    </div>
   )
 }
 
