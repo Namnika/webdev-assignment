e55bb28588dad9aa93d32f615513b538b9fc99af
Namnika <janbandhunamnika@gmail.com>
2025-09-01 00:45:05 +0530

    feat: add avatar image

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import AvatarChart from '../components/charts/AvatarChart'
 import UsersPerDayChart from '../components/charts/UsersPerDayChart'
@@ -8,13 +8,30 @@ import ChartCard from '../components/ChartCard'
 import RecentlyJoinedChart from '../components/charts/RecentlyJoinedChart'
 
 function Dashboard() {
+
+    const [totalUsers, setTotalUsers] = useState([])
+
+    const handlegetTotalUsers = async() => {
+        await fetch("https://6874ce63dd06792b9c954fc7.mockapi.io/api/v1/users")
+        .then(res => res.json())
+        .then(data => setTotalUsers(data))
+        .catch(err => console.log("Unable to fetch Users data", err))
+    }
+    
+    const avatar = totalUsers.map((a) => a.avatar)[0]
+   
+    useEffect(() => {
+         handlegetTotalUsers()
+    }, [])
+    
+
     return (
         <div className='d-flex flex-column mb-3'>
-            <div class="flex-grow-1 ">
-                <Navbar />
+            <div className="flex-grow-1 ">
+                <Navbar avatar={avatar}/>
             </div>
             <div className=' text-center d-flex flex-col align-items-center justify-content-center  mt-3 m-2'>
-                <UserCard title="Total Users" value="1,452" />
+                <UserCard title="Total Users" value={totalUsers.length} />
             </div>
             <div className='container text-center gap-3'>
                 <div className='row'>
