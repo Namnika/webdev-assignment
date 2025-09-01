8650f77a398834f4f2fed7a306bf87b6765bada3
Namnika <janbandhunamnika@gmail.com>
2025-09-01 18:30:04 +0530

    feat: add user's modal (open and close)

--- src/components/UserModal.js
+++ src/components/UserModal.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
-function UserModal() {
+function UserModal({ handleClose, openModal, selectUser }) {
+  if(!openModal) return null
   return (
-    <div>UserModal</div>
+    <div className='modal-overlay'>
+      <div className='user-modal'>
+        <h2 className='fw-bold mb-4 text-xl'>Users Details</h2>
+        {/* {user details} */}
+        {selectUser && (
+          <div key={selectUser.id}>
+            <div className='modal-header' style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "start", gap: "20px"}}>
+              <span><img alt='user-avatar' className='user-avatar' src={selectUser.avatar} /></span>
+              <p><strong>Name: </strong>{selectUser.name}</p>
+            </div>
+            <p><strong>Id: </strong>{selectUser.id}</p>
+            <p><strong>Gender: </strong>{selectUser.gender}</p>
+            <p><strong>Age: </strong>{selectUser.age}</p>
+            <p><strong>Location: </strong>{selectUser.location}</p>
+          </div>
+        )}
+        <button onClick={handleClose} className='modal-close'>X</button>
+      </div>
+    </div>
   )
 }
 
--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,43 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserModal from './UserModal'
 
 function UsersTable({ userList }) {
+
+    const [openModal, setOpenModal] = useState(false)
+    const [currentUser, setCurrentUser] = useState(null)
+
+    const handleOpen = (user) => {
+        setCurrentUser(user)
+        setOpenModal(true)
+    }
+    const handleClose = () => {
+        setCurrentUser(null)
+        setOpenModal(false)
+    }
+
     return (
-        <table className='table table-striped w-100 p-1 table-secondary'>
-            <thead>
-                <tr>
-                    <th scope='col'>ID</th>
-                    <th scope='col'>NAME</th>
-                    <th scope='col'>GENDER</th>
-                    <th scope='col'>LOCATION</th>
-                    <th scope='col'>AGE</th>
-                    <th scope='col'>JOINED DATE</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                {userList.map((u) => (
-                    <tr className='shadow-sm' key={u.id}>
-                        <th scope='row'>{u.id}</th>
-                        <td>
-                            {u.name}
-                        </td>
-                        <td>
-                            {u.gender}
-                        </td>
-                        <td>
-                            {u.location}
-                        </td>
-                        <td>
-                            {u.age}
-                        </td>
-                        <td>
-                            {new Date(u.createdAt).toLocaleDateString()}
-                        </td>
+        <>
+            <table className='table table-striped w-100 p-1 table-secondary'>
+                <thead>
+                    <tr>
+                        <th scope='col'>ID</th>
+                        <th scope='col'>NAME</th>
+                        <th scope='col'>GENDER</th>
+                        <th scope='col'>LOCATION</th>
+                        <th scope='col'>AGE</th>
+                        <th scope='col'>JOINED DATE</th>
                     </tr>
-                ))}
+                </thead>
 
-            </tbody>
-        </table>
+                <tbody>
+                    {userList.map((u) => (
+                        <tr className='shadow-sm' style={{cursor: "pointer"}} onClick={() => handleOpen(u)} key={u.id}>
+                            <th scope='row'>{u.id}</th>
+                            <td>
+                                {u.name}
+                            </td>
+                            <td>
+                                {u.gender}
+                            </td>
+                            <td>
+                                {u.location}
+                            </td>
+                            <td>
+                                {u.age}
+                            </td>
+                            <td>
+                                {new Date(u.createdAt).toLocaleDateString()}
+                            </td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+
+            {<UserModal openModal={openModal} selectUser={currentUser} handleClose={handleClose} />}
+        </>
     )
 }
 
